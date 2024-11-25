225e87c0a81443e4a1e35803f1e668dcfbb0cee1
orlando-santos <santos.orlando.martin@gmail.com>
2024-11-25 16:50:06 +0000

    :construction: Create ProductDTO

---
+++ src/main/java/klx/tech/community/workshop/dto/ProductDTO.java
@@ -0,0 +1,20 @@
+package klx.tech.community.workshop.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder 
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Double discount;
+    private Boolean favorite;
+    private String imageBase64; // Base64 image
+}
