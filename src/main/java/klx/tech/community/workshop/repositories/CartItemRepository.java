8ad0915b012353593391294ad9ea519eb2df5dc5
orlando-santos <santos.orlando.martin@gmail.com>
2024-11-25 16:54:25 +0000

    :construction: Create CartItemProduct entity

---
+++ src/main/java/klx/tech/community/workshop/repositories/CartItemRepository.java
@@ -0,0 +1,10 @@
+package klx.tech.community.workshop.repositories;
+
+import klx.tech.community.workshop.entities.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {}
+    
+
