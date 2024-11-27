cdad85da52241e6a17c56b82a8093ee17c10a5be
orlando-santos <santos.orlando.martin@gmail.com>
2024-11-27 10:09:25 +0000

    :art: Improve format code

--- src/main/java/klx/tech/community/workshop/services/CartItemService.java
+++ src/main/java/klx/tech/community/workshop/services/CartItemService.java
@@ -1,18 +1,23 @@
 package klx.tech.community.workshop.services;
 
+import java.util.List;
+
+import klx.tech.community.workshop.dto.CartItemDTO;
+import klx.tech.community.workshop.dto.CartItemRequestDTO;
 import klx.tech.community.workshop.dto.ProductCartItemDTO;
 import klx.tech.community.workshop.entities.CartItem;
-import klx.tech.community.workshop.entities.Product;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 public interface CartItemService {
-    List<CartItem> findAll();
-    Optional<CartItem> findById(Long id);
-    CartItem create(CartItem cartItem);
-    CartItem update(Long id, Map<Long, Integer> updatedProducts);
+
+    ProductCartItemDTO findById(Long id);
+
+    List<ProductCartItemDTO> findAll();
+
+    CartItem create(CartItemRequestDTO request);
+
+    CartItem update(Long id, CartItemRequestDTO request);
+
     void delete(Long id);
-    ProductCartItemDTO toProductCartItemDTO(Product product, Integer quantity);
+
+    CartItemDTO toCartItemDTO(CartItem cartItem);
 }
