48e2c833701236d8cb31762248f6d8b7e45544bb
orlando-santos <santos.orlando.martin@gmail.com>
2024-11-30 11:05:10 +0000

    :wrench: Add some properties to application.yaml

---
+++ src/main/java/klx/tech/community/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package klx.tech.community.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+
+@OpenAPIDefinition(
+    info = @Info(
+        contact = @Contact(
+            name = "Full Stack K-Tech Community",
+            email = "orlando.santos@klx.com",
+            url = "https://www.klx.pt/"
+        ),
+        description = "Spring Boot backend for store application",
+        title = "Store Backend Server",
+        version = "1.0.0"
+    ),
+    servers = 
+        @Server(
+            description = "Local Environment",
+            url = "http://localhost:8080"
+        )
+)
+public class OpenApiConfig {
+
+}
