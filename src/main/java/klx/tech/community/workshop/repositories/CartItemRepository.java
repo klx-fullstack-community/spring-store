31b29d6f7b8474c92b4d465213238f73e78ec918
orlando-santos <santos.orlando.martin@gmail.com>
2024-11-30 11:03:51 +0000

    :recycle: Use ProductKey to set JpaRepository

--- src/main/java/klx/tech/community/workshop/repositories/CartItemRepository.java
+++ src/main/java/klx/tech/community/workshop/repositories/CartItemRepository.java
@@ -1,10 +1,12 @@
 package klx.tech.community.workshop.repositories;
 
 import klx.tech.community.workshop.entities.CartItem;
+import klx.tech.community.workshop.entities.ProductKey;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {}
+public interface CartItemRepository extends JpaRepository<CartItem, ProductKey> {}
     
 
