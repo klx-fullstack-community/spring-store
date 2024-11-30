7fba510d99a9e275683e5723ee7eae8ab83f8daa
orlando-santos <santos.orlando.martin@gmail.com>
2024-11-30 19:48:39 +0000

    :sparkles: Create CorsConfig class

--- src/main/java/klx/tech/community/workshop/controllers/CartItemController.java
+++ src/main/java/klx/tech/community/workshop/controllers/CartItemController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cart-items")
-@CrossOrigin(origins = "*")
 public class CartItemController {
 
     private final CartItemService cartItemService;
