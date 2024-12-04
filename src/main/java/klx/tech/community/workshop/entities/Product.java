21bc8c97ed537a18c199b9f3f676908c4f579552
klx-fullstack-community <thiago.assis@klx.pt>
2024-12-04 15:01:05 +0000

    :construction: Add constructor with 5 parameters

--- src/main/java/klx/tech/community/workshop/entities/Product.java
+++ src/main/java/klx/tech/community/workshop/entities/Product.java
@@ -1,13 +1,17 @@
 package klx.tech.community.workshop.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor // Constructor sin argumentos (Lombok)
+@AllArgsConstructor // Constructor con todos los argumentos (Lombok)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true) // Only include in specific attributes
-@Table(name = "product")
+@Table(name = "products")
 public class Product {
 
     @Id
@@ -25,12 +29,21 @@ public class Product {
     private Double price;
 
     @Column(nullable = true)
-    private String imageUrl; 
+    private String imageUrl;
 
     @Column(nullable = true)
     private Double discount;
 
     @Column(nullable = false)
     private Boolean favorite;
+
+    public Product(String name, String description, Double price, String imageUrl, Double discount, Boolean favorite) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.discount = discount;
+        this.favorite = favorite;
+    }
 
 }
