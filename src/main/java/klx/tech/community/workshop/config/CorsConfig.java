813a3b010ce75871daaf5b9f2919d1abf219b86d
klx-fullstack-community <thiago.assis@klx.pt>
2024-12-04 14:40:11 +0000

    :sparkles: Create CorsConfig class

---
+++ src/main/java/klx/tech/community/workshop/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package klx.tech.community.workshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @SuppressWarnings("null")
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todas las rutas
+                        .allowedOrigins("http://localhost:3000", "http://localhost:4200", "http://localhost:5000") // Dominio del frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Permite todos los encabezados
+                        .allowCredentials(true); // Permitir cookies si es necesario
+            }
+        };
+    }
+}
